0a207bc4a2abc34d97eee130908357a165dcdf26
Daniel Penner <danielwpenner@shaw.ca>
2025-09-17 20:36:51 -0600

    Changes

--- sql_generation.mjs
+++ sql_generation.mjs
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 import fs from "node:fs";
 
-const schemas = JSON.parse(fs.readFileSync("input_data/dev_tables.json", "utf-8"));
+const schemas = JSON.parse(fs.readFileSync("input_data/dev_tables.json"));
 
 const client = new OpenAI({
     apiKey: "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"
@@ -15,16 +15,15 @@ function returnSchema(id){
 async function prompt(query, schema){
     const response = await client.responses.create({
         model: "gpt-5-mini",
-        input: "Translate the following natural language question into an SQL query for a database with the provided schema. Do not respond with anything except SQL. \nSchema: " + schema + "\nQuestion: " + query
+        input: "Translate the following natural language question into an SQLlite SQL query for a database with the provided schema. Do not respond with anything except SQL. DO NOT use any newlines. All SQL should be on a single line. \nSchema: " + schema + "\nQuestion: " + query
     });
-    console.log(response.output_text);
     return response.output_text;
 }
 
 (async () => {
 try{
     const predictions = [];
-    const queries = JSON.parse(fs.readFileSync("input_data/queries.json", "utf-8"));
+    const queries = JSON.parse(fs.readFileSync("input_data/queries.json"));
     for(const query of queries){
         const sql = await prompt(query.question, returnSchema(query.db_id));
         console.log(sql);
